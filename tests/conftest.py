ad845d26d8f0e6938c38c83df4489b35252a3ea8
mohmdloai <mhmd.00.eng@gmail.com>
2025-07-02 14:55:59 +0300

    Configure: pytest fixture

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    })
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+        yield client
+        with app.app_context():
+            db.drop_all()
