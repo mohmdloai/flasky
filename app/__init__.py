09c51af7e30fe2fdee3586facdb92e9eaa24da35
mohmdloai <mhmd.00.eng@gmail.com>
2025-07-02 20:07:14 +0300

    Include: email configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_mail import Mail
+from config import Config
+
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///inventory.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+    app.config.from_object(Config)
+    
     db.init_app(app)
+    mail.init_app(app)
 
     # Import and register routes
     from . import routes
