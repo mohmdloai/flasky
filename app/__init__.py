6475335db49efcf06db49ced24c25ea5d128c413
mohmdloai <mhmd.00.eng@gmail.com>
2025-09-20 00:48:00 +0300

    feat(cors): add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 from flask_mail import Mail
 from config import Config
 
@@ -12,7 +12,8 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///inventory.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config.from_object(Config)
-    
+    CORS(app, origins=["http://localhost:5173", "http://127.0.0.1:5173"])
+
     db.init_app(app)
     mail.init_app(app)
 
